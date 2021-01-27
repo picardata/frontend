520a0e404dbbba77dcd7a21edd1c49c5ec705016
Vernal <vernal@emewlabs.com>
2021-01-27 08:25:10 +0800

    MASTER: Added babel compact

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,6 +60,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    babel: {
+      compact: true
+    }
   },
   env: {
     apiUrl: 'http://api.local.picardata.com'
