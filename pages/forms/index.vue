310bcbe221d0b061048dd6327342c2f552e4aa8a
Vernal <vernal@emewlabs.com>
2021-03-17 07:47:32 +0800

    ARGON: Added form layout fixes

---
+++ pages/forms/index.vue
@@ -0,0 +1,344 @@
+<template>
+  <div>
+    <base-header type="white" class="pb-6">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+            <route-breadcrumb :crumbs="crumbs" />
+          </nav>
+        </div>
+      </div>
+    </base-header>
+    <div class="container-fluid mt--6">
+      <div class="row mt-3">
+        <div class="col-4">
+          <h1>Forms</h1>
+        </div>
+        <div class="col-8">
+          <span class="align-middle float-right">
+            <nuxt-link to="/forms/new" class="btn btn-lg btn-primary btn-add">Add blank form</nuxt-link>
+          </span>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="form-group col-12">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span id="inputGroupPrepend2" class="input-group-text border-0">
+                <font-awesome-icon
+                  class="search-icon"
+                  fixed-width
+                  size="2x"
+                  :icon="['fas', 'search']"
+                /></span>
+            </div>
+            <input
+              v-model="qSearch"
+              type="text"
+              class="form-control search-box border-0"
+              placeholder="Search created forms"
+              aria-describedby="inputGroupPrepend2"
+              @keyup="querySearch"
+            >
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-4">
+          <h4>All forms</h4>
+        </div>
+        <div class="col-8">
+          <div class="dropdown col-3 fa-pull-right">
+            <button
+              v-if="sort === 0"
+              class="btn dropdown-toggle text-primary"
+              type="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              <font-awesome-icon
+                fixed-width
+                :icon="['fas', 'sort-amount-down-alt']"
+              />
+              Last updated
+            </button>
+            <button
+              v-if="sort === 1"
+              class="btn dropdown-toggle text-primary"
+              type="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              <font-awesome-icon
+                fixed-width
+                :icon="['fas', 'sort-amount-down-alt']"
+              />
+              Title
+            </button>
+            <div class="dropdown-menu" aria-labelledby="sort-dropdown">
+              <button
+                v-if="sort === 1"
+                class="dropdown-item font-weight-bold fa-pull-left text-primary"
+                type="button"
+                @click.prevent="toggleSort"
+              >
+                <font-awesome-icon
+                  fixed-width
+                  :icon="['fas', 'sort-amount-down-alt']"
+                />
+                Last updated
+              </button>
+              <button
+                v-if="sort === 0"
+                class="dropdown-item font-weight-bold fa-pull-left text-primary"
+                type="button"
+                @click.prevent="toggleSort"
+              >
+                <font-awesome-icon
+                  fixed-width
+                  :icon="['fas', 'sort-amount-down-alt']"
+                />
+                Title
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div v-for="(form, index) in data" :key="form.id" class="p-4 col-md-4 col-sm-12">
+          <div class="card pb-4" @dblclick="$router.push(openLink(form.id))">
+            <div class="card-body">
+              <h5 class="card-title">
+                {{ form.name }}
+              </h5>
+              <div class="row">
+                <div class="col-2 pt-3 pl-4">
+                  <font-awesome-icon
+                    fixed-width
+                    size="lg"
+                    class="sync-icon"
+                    :icon="['fas', 'sync']"
+                  />
+                </div>
+                <div class="col-10">
+                  <p class="card-text last-updated">
+                    Last updated:<br>
+                    {{ formatDate(form.updatedAt) }}
+                  </p>
+                </div>
+              </div>
+              <div class="row mt-2">
+                <div class="col-md-4 col-sm-12">
+                  <nuxt-link class="btn btn-gray-light" to="/forms/share">
+                    <span class="text-primary">Share</span>
+                  </nuxt-link>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                  <div class="divider d-inline"></div>
+                  <nuxt-link class="btn btn-gray-light" :to="openLink(form.id)">
+                    <span class="text-primary">Open</span>
+                  </nuxt-link>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                  <div class="divider d-inline"></div>
+                  <a class="btn btn-gray-light" href="#" @click.prevent="deletePop(index)">
+                    Delete
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <modal name="delete-modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <h5>Move to Trash?</h5>
+              <div class="cancel-integrate" @click="dismissModal">
+                &times;
+              </div>
+            </div>
+            <div class="modal-body">
+              <p>"{{ selectedDeletion.name }}" will be deleted forever.</p>
+            </div>
+            <div class="modal-footer">
+              <a href="#" class="btn btn-default" @click.prevent="dismissModal">Cancel</a>
+              <a href="#" class="btn btn-primary" @click.prevent="deleteConfirm">Move to Trash</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const days = ['Mon', 'Tue', 'Thu', 'Fri', 'Sat', 'Sun']
+
+export default {
+  name: 'IndexVue',
+  auth: true,
+  layout: 'argon',
+  async asyncData (context) {
+    return await context.app.$axios.get('/api/forms/', { params: { 'order[updatedAt]': 'desc' } })
+      .then((data) => {
+        console.log(data)
+        return { data: data.data }
+      })
+  },
+  data () {
+    return {
+      crumbs: [
+        {
+          name: 'Forms',
+          path: '/forms'
+        }
+      ],
+      sort: 0,
+      qSearch: '',
+      selectedDeletion: {
+        id: 0,
+        name: 'Untitled Form'
+      }
+    }
+  },
+  computed: {
+    sortParams () {
+      const params = {}
+      if (this.sort === 1) {
+        params['order[name]'] = 'asc'
+      } else {
+        params['order[updatedAt]'] = 'desc'
+      }
+      params.q = this.qSearch
+      return params
+    }
+  },
+  methods: {
+    openLink (id) {
+      return '/forms/' + id
+    },
+    formatDate (date) {
+      const dateTime = new Date(date)
+      return days[dateTime.getDay()] + ', ' + dateTime.getDate() + ' ' + months[dateTime.getMonth()] + ' ' + dateTime.getFullYear()
+    },
+    toggleSort () {
+      if (this.sort === 0) {
+        this.sort = 1
+      } else {
+        this.sort = 0
+      }
+      this.$axios.get('/api/forms/', {
+        params: this.sortParams
+      })
+        .then((data) => {
+          console.log(data)
+          this.data = data.data
+        })
+    },
+    search () {
+      this.$axios.get('/api/forms/', {
+        params: this.sortParams
+      })
+        .then((data) => {
+          console.log(data)
+          this.data = data.data
+        })
+    },
+    querySearch () {
+      if (this.qSearch.length > 2) {
+        this.search()
+      }
+    },
+    deletePop (index) {
+      this.selectedDeletion = this.data[index]
+      this.$modal.show('delete-modal')
+    },
+    dismissModal () {
+      this.$modal.hide('delete-modal')
+    },
+    deleteConfirm () {
+      this.$axios
+        .$delete('/api/forms/' + this.selectedDeletion.id)
+        .then((data) => {
+          // eslint-disable-next-line no-console
+          console.log(data)
+          this.search()
+          this.$modal.hide('delete-modal')
+        })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 36px;
+  font-weight: bolder;
+}
+
+.btn-add {
+  height: 56px;
+  border-radius: 40px;
+  font-weight: normal;
+  font-size: 16px;
+  padding: 15px 30px;
+}
+
+.search-box {
+  height: 62px;
+  border-radius: 16px;
+  background-color: #EFF0F6;
+  padding-left: 0px;
+}
+
+span.input-group-text {
+  background-color: #EFF0F6;
+  border-radius: 16px;
+}
+
+.search-icon, .sync-icon {
+  color: #A0A3BD;
+}
+
+div.dropdown button {
+  background-color: white;
+  border: 0;
+}
+
+div.dropdown button.dropdown-toggle:focus, div.dropdown button.dropdown-toggle:active {
+  background-color: white;
+  border-color: white;
+  box-shadow: none;
+}
+
+.last-updated {
+  color: #313131;
+  font-size: 14px;
+  opacity: 0.6;
+}
+
+div.dropdown button.dropdown-item {
+  padding-left: 10px;
+}
+
+.dropdown-toggle:after {
+  content: none
+}
+
+div.vl {
+  border-left: 2px solid #C4C4C4;
+  margin-top: 10px;
+  padding-top: 0;
+  height: 16px;
+}
+
+</style>
---
+++ pages/forms/new/index.vue
@@ -0,0 +1,446 @@
+<template>
+  <div>
+    <base-header type="white" class="pb-6">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+            <route-breadcrumb :crumbs="crumbs" />
+          </nav>
+        </div>
+      </div>
+    </base-header>
+    <div class="container-fluid mt--6">
+      <div class="row mt-3">
+        <div class="col-6">
+          <h1>Create blank form</h1>
+        </div>
+        <div class="col-6">
+          <span class="align-middle float-right">
+            <nuxt-link to="/forms/new" class="btn btn btn-outline-primary btn-create">Create other blank form</nuxt-link>
+          </span>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-4">
+          <h5><b>Questions</b></h5>
+        </div>
+        <div class="col-8">
+          <span class="align-middle float-right">
+            <nuxt-link to="/form/preview" class="btn text-primary btn-lg bg-white btn-preview">
+              <font-awesome-icon :icon="['fas', 'eye']" />
+              Preview form</nuxt-link>
+            <nuxt-link to="/form/share" class="btn btn-lg btn-primary btn-share">Share form</nuxt-link>
+          </span>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <form>
+          <div class="card">
+            <div class="card-body">
+              <div>
+                <hr class="header-break">
+              </div>
+              <div class="form-group">
+                <input
+                  v-model="name"
+                  type="text"
+                  name="name"
+                  class="form-control title mat border-0"
+                  placeholder="Untitled form"
+                  required="required"
+                  @change="submit"
+                >
+              </div>
+              <div class="form-group">
+                <input
+                  v-model="description"
+                  type="text"
+                  name="description"
+                  class="form-control description mat border-0"
+                  placeholder="Form description"
+                  @change="submit"
+                >
+              </div>
+            </div>
+          </div>
+          <Field
+            :questions="questions"
+            :add_field="addField"
+            :change_type="changeType"
+            :copy_field="copyField"
+            :delete_field="deleteField"
+          />
+        </form>
+      </div>
+      <div class="stick-bottom">
+        <button
+          class="btn btn-primary btn-md "
+          type="button"
+          @click="newField"
+        >
+          <font-awesome-icon :icon="['fas', 'plus']" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Field from '@/components/Field/Field'
+
+export default {
+  name: 'IndexVue',
+  layout: 'argon',
+  components: {
+    Field
+  },
+  data () {
+    return {
+      crumbs: [
+        {
+          name: 'Forms',
+          path: '/forms'
+        },
+        {
+          name: 'New Form',
+          path: '/forms/new'
+        }
+      ],
+      id: '',
+      name: 'Untitled form',
+      description: '',
+      questions: [
+        {
+          id: undefined,
+          name: '',
+          type: 0,
+          required: false,
+          fieldChoice: [
+            {
+              id: undefined,
+              type: 1,
+              name: 'Option 1',
+              edit: false,
+              alert: ''
+            },
+            {
+              id: undefined,
+              type: 1,
+              name: 'Add option',
+              edit: false,
+              alert: ''
+            }
+          ]
+        }
+      ]
+    }
+  },
+  computed:
+    {
+      questionsLength () {
+        return this.questions.length
+      }
+    },
+  methods: {
+    async submitField (index, formId) {
+      const fieldId = this.questions[index].id ? this.questions[index].id : undefined
+      const toSave = {
+        name: this.questions[index].name ? this.questions[index].name : 'Question',
+        type: this.questions[index].type,
+        required: this.questions[index].required,
+        form: formId
+      }
+      let axios
+
+      if (fieldId) {
+        axios = this.$axios.$put('/api/fields/' + fieldId, toSave)
+      } else {
+        axios = this.$axios.$post('/api/fields/', toSave)
+      }
+
+      await axios.then((data) => {
+        this.questions[index].id = data.id
+        if (!this.id) {
+          this.id = data.form.id
+          history.pushState(
+            {},
+            null,
+            this.$route.path.replace('new', data.form.id)
+          )
+        }
+      })
+        .catch((e) => {
+          this.errors = []
+          for (const field of ['username', 'password']) {
+            const errors = e.response.data.errors[field]
+            if (errors !== undefined) {
+              this.errors = this.errors.concat(errors)
+            }
+          }
+          return false
+        })
+    },
+    async addField (index) {
+      if (this.id === '') {
+        await this.submit().then(() => {
+          this.submitField(index, this.id)
+        })
+      } else {
+        await this.submitField(index, this.id)
+      }
+    },
+    changeType (questionId, typeId) {
+      this.questions[questionId].type = typeId
+      this.bulkDeleteFieldChoice(questionId)
+
+      this.submit().then(() => {
+        this.submitField(questionId, this.id).then(() => {
+          if (typeId > 1) {
+            this.addChoices(questionId)
+          }
+        })
+      })
+    },
+    bulkDeleteFieldChoice (questionId) {
+      this.questions[questionId].fieldChoice.map((x) => {
+        if (x.id) {
+          this.$axios.$delete('/api/field-choices/' + x.id)
+        }
+      })
+
+      this.questions[questionId].fieldChoice = [
+        {
+          id: undefined,
+          type: 1,
+          name: 'Option 1',
+          edit: false,
+          alert: ''
+        },
+        {
+          id: undefined,
+          type: 1,
+          name: 'Add option',
+          edit: false,
+          alert: ''
+        }
+      ]
+    },
+    newField () {
+      this.questions.push({
+        id: undefined,
+        name: '',
+        type: 0,
+        required: false,
+        fieldChoice: [
+          {
+            id: undefined,
+            order: 0,
+            type: 1,
+            name: 'Option 1',
+            edit: false,
+            alert: ''
+          },
+          {
+            id: undefined,
+            type: 1,
+            name: 'Add option',
+            edit: false,
+            alert: ''
+          }
+        ]
+      })
+      this.submitField(this.questionsLength - 1, this.id).then(() => {
+        if (this.questions[this.questionsLength - 1].type > 1) {
+          this.addChoices(this.questionsLength - 1)
+        }
+      })
+    },
+    copyField (index) {
+      const toCopy = this.questions[index]
+      const copied = {
+        id: undefined,
+        name: toCopy.name,
+        type: toCopy.type,
+        required: toCopy.required,
+        fieldChoice: this.copyChoices(toCopy.fieldChoice)
+      }
+      this.questions.splice(index + 1, 0, copied)
+      this.addField(index + 1).then(() => {
+        if (copied.type > 1) {
+          this.addChoices(index + 1)
+        }
+      })
+    },
+    copyChoices (choices) {
+      return choices.map((v) => {
+        return {
+          id: undefined,
+          type: v.type,
+          name: v.name,
+          edit: false,
+          alert: v.alert
+        }
+      })
+    },
+    addChoices (index) {
+      const lastIndex = this.questions[index].fieldChoice.length - 1
+      this.questions[index]
+        .fieldChoice
+        .map((v, i) => {
+          if (i < lastIndex) {
+            this.$axios.$post('/api/field-choices/', {
+              name: v.name,
+              type: v.type,
+              field: this.questions[index].id,
+              choice_order: i
+            })
+              .then((res) => {
+                v.id = res.id
+              })
+          }
+        })
+    },
+    deleteField (index) {
+      this.$axios.$delete('/api/fields/' + this.questions[index].id)
+      this.questions.splice(index, 1)
+    },
+    async submit () {
+      if (this.id === '') {
+        await this.$axios
+          .$post('/api/forms/', {
+            name: this.name,
+            description: this.description
+          })
+          .then((data) => {
+            // eslint-disable-next-line no-console
+            console.log(data)
+            this.id = data.id
+            history.pushState(
+              {},
+              null,
+              this.$route.path.replace('new', this.id)
+            )
+          })
+          .catch((e) => {
+            this.errors = []
+            for (const field of ['username', 'password']) {
+              const errors = e.response.data.errors[field]
+              if (errors !== undefined) {
+                this.errors = this.errors.concat(errors)
+              }
+            }
+            return false
+          })
+      } else {
+        await this.$axios
+          .$patch('/api/forms/' + this.id, {
+            name: this.name,
+            description: this.description
+          })
+          .then((data) => {
+            // eslint-disable-next-line no-console
+            console.log(data)
+            this.id = data.id
+          })
+          .catch((e) => {
+            this.errors = []
+            for (const field of ['username', 'password']) {
+              const errors = e.response.data.errors[field]
+              if (errors !== undefined) {
+                this.errors = this.errors.concat(errors)
+              }
+            }
+            return false
+          })
+      }
+    }
+
+  }
+}
+</script>
+<style scoped>
+.stick-bottom {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin-right: 240px;
+  margin-bottom: 70px;
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: bolder;
+}
+
+.btn-create, .btn-share, .btn-preview {
+  height: 56px;
+  border-radius: 40px;
+  border-width: 2px;
+  font-size: 16px;
+  padding: 15px 15px;
+}
+
+.btn-create {
+  padding: 15px 15px;
+}
+
+.btn-share, .btn-preview {
+  padding: 15px 30px;
+}
+
+form {
+  width: 100%;
+}
+
+.form-control {
+  color: black;
+}
+
+input.title {
+  font-size: 48px;
+}
+
+input.description {
+  font-size: 18px;
+}
+
+input.question:focus {
+  border-color: var(--primary);
+}
+
+.type-dropdown {
+  padding: 0;
+}
+
+.type-dropdown .btn {
+  height: 56px;
+  text-align: left;
+  border-radius: 15px;
+  background-color: #EFF0F7;
+  font-size: 16px;
+}
+
+input.question {
+  font-size: 24px;
+  border-width: 0 0 2px 0;
+  border-radius: 0;
+}
+
+.form-control:focus {
+  box-shadow: none;
+}
+
+hr.header-break {
+  border-top: 8px solid var(--blue);
+  margin-left: 15px;
+  width: 130px;
+}
+
+.dropdown-toggle::after {
+  display: none;
+}
+
+.form-control:focus {
+  box-shadow: none;
+}
+</style>
