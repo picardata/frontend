041c5216df0b46690fa47f28ea7020eb75ae0f99
Moch Deden <moch.deden@emewlabs.com>
2021-04-22 12:58:59 +0700

    Update back arrow

--- pages/apps/app-library/index.vue
+++ pages/apps/app-library/index.vue
@@ -14,9 +14,9 @@
         <div class="col-sm-6 float-right picardata-title">
           <div class="row ">
             <!-- <div class="col-sm-1"></div> -->
-            <div class="col-sm-1" style="color: #313131">
-              <a class="pd-icon pdicon-Back-Arrow" href="/apps" style="color: #313131" />
-            </div>
+            <a class="pd-icon pdicon-Back-Arrow picardata-arrow" href="/apps" style="color: #313131" />
+            <!-- <div class="col-sm-1" style="color: #313131">
+            </div> -->
             <div class="col-sm-11">
               App Library
             </div>
@@ -621,5 +621,10 @@ letter-spacing: 0.75px;
 
 color: #2534B6;
 
+}
+
+.picardata-arrow {
+  color: #14142B;
+  margin: auto
 }
 </style>
