33241c56cc4367b67434aeb19a8318295fac9019
Moch Deden <moch.deden@emewlabs.com>
2021-04-25 08:57:53 +0700

    Init

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -1,70 +1,82 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-12 mt-5">
-        <h3><img src="~/assets/nav_logo_dark.png" alt="Picardata"> How Picardata works?</h3>
-      </div>
-      <div class="col-8 mt-2">
-        <div class="card-deck">
-          <div class="card">
-            <div class="card-body p-4">
-              <h5 class="card-title text-highlight">
-                YOUR APPS
-              </h5>
-              <p class="card-text">
-                You probably have a tons of app to manage. Social media, design tools, sales and marketing and much more.
-              </p>
+  <div class="bg-outer">
+    <div class="wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="icon-picardata text-center">
+              <img class="mb-2" src="~/assets/logo.png" alt="">
+              <h2>Login to Picardata</h2>
             </div>
-          </div>
-          <div class="card">
-            <div class="card-body p-4">
-              <h5 class="card-title text-highlight">
-                HANDLE YOUR APPS
-              </h5>
-              <p class="card-text">
-                How do you manage your apps precisely? Picardata can do that for you to monitor and view how they work.
-              </p>
+
+            <div class="form-group mt-4">
+              <label
+                :class="[`form-control-label`, {'d-none': !errors.email}]"
+              >
+                Email
+              </label>
+              <input
+                v-model="email"
+                :class="[`form-control`, 'login-credential-input', {'error': errors.email}]"
+                placeholder="Email"
+                @change="validateEmail"
+              >
+              <span class="form-icon" @click="emptyInput('email')"><i class="fa fa-times" /></span>
+              <span
+                :class="['form-control-error', {'d-none': !errors.email}]"
+              >
+                {{ errors.email }}
+              </span>
             </div>
-          </div>
-          <div class="card">
-            <div class="card-body p-4">
-              <h5 class="card-title text-highlight">
-                INTEGRATE YOUR APP
-              </h5>
-              <p class="card-text">
-                Firstly you have to integrate them. You can add, revoke and view data and access within your app.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-4 mt-2">
-        <div class="card align-self-center">
-          <div class="card-body">
-            <form @submit.prevent="onSubmit">
-              <p v-if="errors.length" class="text-danger">
-                <b>Please correct the following error(s):</b>
-              </p>
-              <ul class="text-danger">
-                <li v-for="error in errors" :key="error">
-                  {{ error }}
-                </li>
-              </ul>
-              <AppControlInput v-model="email" required type="email">
-                E-Mail Address
-              </AppControlInput>
-              <AppControlInput v-model="password" required type="password">
+
+            <div class="form-group mt-4">
+              <label
+                :class="[`form-control-label`, {'d-none': !errors.password}]"
+              >
                 Password
-              </AppControlInput>
-              <div class="form-submission text-right">
-                <AppButton class="btn btn-primary" type="submit">
-                  {{ isLogin ? 'Login' : 'Sign Up' }}
-                </AppButton>
-              </div>
-            </form>
-            <div class="row mt-2 p-3">
-              Already have an account? &nbsp;<nuxt-link to="/login">
-                Log In
+              </label>
+              <input
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                :class="[`form-control`, 'login-credential-input', {'error': errors.password}]"
+                placeholder="Password"
+                @change="validatePassword"
+              >
+
+              <span v-if="showPassword" class="form-icon" @click="togglePassword"><i class="fa fa-eye-slash" /></span>
+              <span v-else class="form-icon" @click="togglePassword"><i class="fa fa-eye" /></span>
+
+              <span
+                :class="[`form-control-error`, {'d-none': !errors.password}]"
+                v-html="errors.password"
+              />
+            </div>
+
+            <div class="form-check  mt-4">
+              <input id="checkbox-remember-me" type="checkbox" class="form-check-input">
+              <label class="form-check-label ml-2" for="checkbox-remember-me">Remember me</label>
+            </div>
+
+            <button
+              type="button"
+              :class="['btn btn-primary btn-block mt-4 mb-4 rounded']"
+              @click="onSubmit"
+            >
+              Login
+            </button>
+
+            <div class="col-md-12 text-center">
+              <nuxt-link to="#">
+                Forgot password
+              </nuxt-link>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="img-banner">
+              <img src="~/assets/register-now.png" alt="">
+
+              <nuxt-link to="/signup" class="btn btn-outline-primary btn-block">
+                Not registered yet? Register now
               </nuxt-link>
             </div>
           </div>
@@ -74,40 +86,54 @@
   </div>
 </template>
 <script>
+
 export default {
   name: 'AdminAuthPage',
-  layout: 'admin',
+  layout: 'empty',
   auth: false,
   data () {
     return {
-      errors: [],
+      errors: {
+        email: '',
+        password: ''
+      },
+      showPassword: false,
       isLogin: false,
       email: '',
       password: ''
     }
   },
+  // beforeMount () {
+  //   return this.$auth.loggedIn ? this.$router.push('/') : ''
+  // },
   methods: {
     async onSubmit () {
+      const isValidate = this.validateEmail() && this.validatePassword()
+      if (!isValidate) {
+        return false
+      }
+
       if (!this.isLogin) {
-        const result = await this.$axios
-          .$post('/api/users/', {
-            username: this.email,
-            password: this.password
-          })
-          .then((data) => {
-            // eslint-disable-next-line no-console
-            console.log(data)
-          })
-          .catch((e) => {
+        let result;
+        try {
+           result = await this.$axios
+                              .$post('/api/users/', {
+                                username: this.email,
+                                password: this.password
+                              })
+        } catch(e) {
+            this.errors = []
             for (const field of ['username', 'password']) {
               const errors = e.response.data.errors[field]
               if (errors !== undefined) {
-                this.errors = errors
+                this.errors = this.errors.concat(errors)
               }
             }
-            return false
-          })
+            result = false
+        }
+
         if (result === false) {
+          this.errors.password = '<i class="fa fa-exclamation-circle"></i> Email or password  you entered is incorrect'
           return false
         }
       }
@@ -124,10 +150,139 @@ export default {
           this.$router.push('/')
         }
       } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(err)
+        this.errors.password = '<i class="fa fa-exclamation-circle"></i> Email or password  you entered is incorrect'
+        return false
       }
+    },
+    validateEmail () {
+      const email = this.email
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      const test = re.test(email.toLowerCase())
+
+      if (!test) {
+        this.errors.email = 'Error email\'s format'
+        return false
+      }
+
+      this.errors.email = ''
+      return true
+    },
+    validatePassword () {
+      const password = this.password
+
+      if (password.length < 8) {
+        this.errors.password = 'Minimum password length 8 chars'
+        return false
+      }
+
+      const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/
+      const test = re.test(password)
+
+      // if (!test) {
+      //   this.errors.password = 'Error password\'s format'
+      //   return false
+      // }
+
+      this.errors.password = ''
+      return true
+    },
+    emptyInput (value) {
+      this[value] = ''
+      this.errors[value] = ''
+    },
+    togglePassword () {
+      this.showPassword = !this.showPassword
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.bg-outer{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-color: #181C3B;
+  padding: 3em 16em;
+}
+
+.wrapper{
+  background: #FFFFFF;
+  width: 100%;
+  height: auto;
+  border-radius: 16px;
+  padding: 3rem 4rem 3rem;
+}
+
+.icon-picardata{
+  h2{
+    margin-top: 24px;
+    margin-bottom: 32px;
+  }
+}
+
+.mt-4{
+  margin-top: 1.25rem;
+}
+
+.mb-4{
+  margin-bottom: 1.25rem;
+}
+
+.form-check-input{
+  transform: scale(1.5);
+}
+
+.btn-primary{
+  background: #2534B6;
+  &.rounded{
+    border-radius: 40px !important;
+  }
+  &:hover{
+    box-shadow: none;
+    transform: translateY(0);
+  }
+}
+
+.img-banner{
+  margin-top: 48px;
+
+  .btn{
+    margin-top: 32px;
+  }
+}
+
+.form-group{
+  position: relative;
+
+  .form-control-label{
+    position: absolute;
+    padding: 6px 24px;
+    color: #C30052;
+    z-index: 2;
+
+    + .login-credential-input{
+      padding: 24px 24px 0;
+    }
+
+    &.d-none + .login-credential-input{
+      padding: 0 24px;
+    }
+  }
+
+  .form-control-error{
+    color: #C30052;
+    margin-top: 8px;
+    font-size: 14px;
+  }
+
+  .form-icon{
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 24px;
+    font-size: 14px;
+    color: #A0A3BD;
+  }
+}
+</style>
