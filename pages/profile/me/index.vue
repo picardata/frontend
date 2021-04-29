a0c576735e0af987e9bdebbe341561d7a4d3e794
Moch Deden <moch.deden@emewlabs.com>
2021-04-29 20:45:44 +0700

    Update style

--- pages/profile/me/index.vue
+++ pages/profile/me/index.vue
@@ -332,4 +332,45 @@ option, .form-control:focus {
   background: #EFF0F7;
   border-radius: 15px;
 }
+
+.login-credential-input {
+  background: #EFF0F7;
+  border-radius: 15px;
+  color: #181C3B;
+  min-height: 64px;
+  border: 1px solid #dee2e6;
+  font-size: 1rem;
+  padding: 0 24px;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+
+  &:focus {
+    color: #181C3B;
+    background-color: #EFF0F7;
+    border: 1px solid #dee2e6;
+    outline: 0;
+    box-shadow: none;
+  }
+
+  &::placeholder {
+    color: #A0A3BD;
+  }
+
+  + .input-group-append {
+    background: #EFF0F7;
+    border-radius: 0 16px 16px 0;
+
+    .input-group-text {
+      border-radius: 0 16px 16px 0;
+      font-size: 1rem;
+    }
+  }
+
+  &.error {
+    background: #EFF0F7;
+    border: 2px solid #ED2E7E;
+  }
+}
 </style>
