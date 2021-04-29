723b07e920781f962d38191f6cf8ee3d928f286a
Ilham <fajarmuhammadilham@gmail.com>
2021-04-29 19:06:10 +0700

    PDS-258: Templating onboarding add profile

--- pages/onboarding/index.vue
+++ pages/onboarding/index.vue
@@ -84,7 +84,42 @@
       </div>
     </div>
     <div v-if="step === 3" class="col-9">
+      <div class="row text-center">
+        <div class="mt-4 col-12">
+          <div class="progress" style="height: 5px;">
+            <div
+              class="progress-bar bg-blue"
+              role="progressbar"
+              style="width: 66%"
+              aria-valuenow="33"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-4">
+          <span class="text-highlight">01.</span> Welcome on board
+        </div>
+        <div class="col-4 font-weight-bold">
+          <span class="text-highlight">02.</span> Complete Profile
+        </div>
+        <div class="col-4">
+          <span class="text-highlight">03.</span> Start Integrating
+        </div>
+      </div>
       <CompleteProfile />
+      <div class="row mt-5 justify-content-end">
+        <div class="pl-2">
+          <button type="button" class="btn btn-link btn-link-dark-gray btn-lg" @click.prevent="post">
+            Skip for now
+          </button>
+          <button type="button" class="btn btn-lg btn-primary btn-add" @click.prevent="next">
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -102,7 +137,7 @@ export default {
   auth: true,
   data () {
     return {
-      step: 3,
+      step:13,
       country: '',
       choices: [
         {
@@ -155,46 +190,44 @@ export default {
   },
   methods: {
     post () {
-      this.$axios.$post('/api/employees/', {
-        userProfile: {
-          firstname: this.profile.name.trim(),
-          lastname: '',
-          address: this.profile.location,
-          phone: this.profile.phone.trim() === '' ? '' : this.profile.formattedPhone,
-          email: this.profile.email,
-          user: this.$auth.user.id
-        },
-        role: this.profile.role,
-        occupation: this.profile.occupation,
-        company: {
-          name: this.profile.organization,
-          location: this.profile.workLocation
-        }
-      }).then((data) => {
-        this.$auth.setUser(data)
-        this.$router.push('/library')
-      }).catch((e) => {
-        const errors = {}
-
-        if (e.response.data.errors.userProfile !== undefined) {
-          Object.entries(e.response.data.errors.userProfile).forEach(function (value) {
-            const key = 'profile.' + value[0]
-            errors[key] = value[1]
-          })
-        }
-        if (e.response.data.errors.company !== undefined) {
-          Object.entries(e.response.data.errors.company).forEach(function (value) {
-            const key = 'company.' + value[0]
-            errors[key] = value[1]
-          })
-        }
-
-        // eslint-disable-next-line no-console
-        console.log(errors)
-        this.$refs.form.setErrors(errors)
-        this.step = 3
-        return false
-      })
+      // this.$axios.$post('/api/employees/', {
+      //   userProfile: {
+      //     firstname: this.profile.name.trim(),
+      //     lastname: '',
+      //     address: this.profile.location,
+      //     phone: this.profile.phone.trim() === '' ? '' : this.profile.formattedPhone,
+      //     email: this.profile.email,
+      //     user: this.$auth.user.id
+      //   },
+      //   role: this.profile.role,
+      //   occupation: this.profile.occupation,
+      //   company: {
+      //     name: this.profile.organization,
+      //     location: this.profile.workLocation
+      //   }
+      // }).then((data) => {
+      //   this.$auth.setUser(data)
+      //   this.$router.push('/library')
+      // }).catch((e) => {
+      //   const errors = {}
+      //
+      //   if (e.response.data.errors.userProfile !== undefined) {
+      //     Object.entries(e.response.data.errors.userProfile).forEach(function (value) {
+      //       const key = 'profile.' + value[0]
+      //       errors[key] = value[1]
+      //     })
+      //   }
+      //   if (e.response.data.errors.company !== undefined) {
+      //     Object.entries(e.response.data.errors.company).forEach(function (value) {
+      //       const key = 'company.' + value[0]
+      //       errors[key] = value[1]
+      //     })
+      //   }
+      //
+      //   this.$refs.form.setErrors(errors)
+      //   this.step = 3
+      //   return false
+      // })
     },
     next () {
       if (this.step === 4) {
