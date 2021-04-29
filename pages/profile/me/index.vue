1428286638a4866e74ef49e6474684d421157da9
Moch Deden <moch.deden@emewlabs.com>
2021-04-29 16:18:57 +0700

    Set margin: auto on cancel button

--- pages/profile/me/index.vue
+++ pages/profile/me/index.vue
@@ -123,7 +123,7 @@
             </div>
           </div>
           <div class="row">
-              <div class="col-sm-4 col-xs-12 text-left" v-if="buttonStatus != 'VIEW'">
+              <div class="col-sm-4 col-xs-12 text-left" v-if="buttonStatus != 'VIEW'" style="margin: auto">
                 <span v-on:click="cancel">Cancel</span>
               </div>
               <div class="col-sm-8 col-xs-12 text-right" v-if="buttonStatus === 'VIEW'">
