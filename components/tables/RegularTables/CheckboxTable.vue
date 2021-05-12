74d76bc1162bb5402b8da1bf51ac78efd0991dd9
aakhyar <akhyar.adhin@emewlabs.com>
2021-05-12 12:20:05 +0700

    PDS-310: manage all forms layout
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>

--- components/tables/RegularTables/CheckboxTable.vue
+++ components/tables/RegularTables/CheckboxTable.vue
@@ -1,79 +1,82 @@
 <template>
   <div class="card">
-    <div class="card-header border-0">
-      <div class="row">
-        <div class="col-6">
-          <h3 class="mb-0">
-            Checkbox + Toggles
-          </h3>
-        </div>
-        <div class="col-6 text-right">
-          <el-tooltip content="Delete" placement="top">
-            <base-button type="danger" icon size="sm">
-              <span class="btn-inner--icon"><i class="fas fa-trash" /></span>
-              <span class="btn-inner--text">Delete</span>
-            </base-button>
-          </el-tooltip>
+    <slot name="table-header">
+      <div class="card-header border-0">
+        <div class="row">
+          <div class="col-6">
+            <h3 class="mb-0">
+              Checkbox + Toggles
+            </h3>
+          </div>
+          <div class="col-6 text-right">
+            <el-tooltip content="Delete" placement="top">
+              <base-button type="danger" icon size="sm">
+                <span class="btn-inner--icon"><i class="fas fa-trash" /></span>
+                <span class="btn-inner--text">Delete</span>
+              </base-button>
+            </el-tooltip>
+          </div>
         </div>
       </div>
-    </div>
-
-    <el-table
-      class="table-responsive table-flush"
-      header-row-class-name="thead-light"
-      row-key="id"
-      :data="users"
-      @selection-change="onSelectionChange"
-    >
-      <el-table-column
-        type="selection"
-        align="left"
-        min-width="120px"
-      />
-
-      <el-table-column
-        label="Author"
-        min-width="220px"
-        prop="name"
-        sortable
+    </slot>    
+    <slot name="table">
+      <el-table
+        class="table-responsive table-flush"
+        header-row-class-name="thead-light"
+        row-key="id"
+        :data="users"
+        @selection-change="onSelectionChange"
       >
-        <template v-slot="{row}">
-          <b>{{ row.name }}</b>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="Created At"
-        prop="createdAt"
-        width="170px"
-        min-width="140px"
-        sortable
-      />
+        <el-table-column
+          type="selection"
+          align="left"
+          min-width="120px"
+        />
 
-      <el-table-column
-        label="Product"
-        min-width="180px"
-        prop="product"
-        sortable
-      >
-        <template v-slot="{row}">
-          <a href="#!" class="font-weight-bold">{{ row.product }}</a>
-        </template>
-      </el-table-column>
+        <el-table-column
+          label="Author"
+          min-width="220px"
+          prop="name"
+          sortable
+        >
+          <template v-slot="{row}">
+            <b>{{ row.name }}</b>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="Created At"
+          prop="createdAt"
+          width="170px"
+          min-width="140px"
+          sortable
+        />
 
-      <el-table-column
-        min-width="120px"
-        label="Active"
-        prop="active"
-        sortable
-        align="center"
-      >
-        <template v-slot="{row}">
-          <div class="d-flex justify-content-center">
-            <base-switch v-model="row.active" />
-          </div>
-        </template>
-      </el-table-column>
-    </el-table>
+        <el-table-column
+          label="Product"
+          min-width="180px"
+          prop="product"
+          sortable
+        >
+          <template v-slot="{row}">
+            <a href="#!" class="font-weight-bold">{{ row.product }}</a>
+          </template>
+        </el-table-column>
+
+        <el-table-column
+          min-width="120px"
+          label="Active"
+          prop="active"
+          sortable
+          align="center"
+        >
+          <template v-slot="{row}">
+            <div class="d-flex justify-content-center">
+              <base-switch v-model="row.active" />
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </slot>    
   </div>
 </template>
 <script>
