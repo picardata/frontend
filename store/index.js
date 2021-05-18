92a1f80a7e3d591cb7a77a959a90561fbe9d3648
aakhyar <akhyar.adhin@emewlabs.com>
2021-05-18 08:32:36 +0700

    PDS-364: add loader mixin & vuex loader state
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>

--- store/index.js
+++ store/index.js
@@ -87,6 +87,27 @@ const createStore = () => {
       isAuthenticated (state) {
         return state.token != null
       }
+    },
+    modules: {
+      loader: {
+        namespaced: true,
+        state: () => ({
+          refCount: 0,
+          isLoading: false,
+        }),
+        mutations: {
+          loading(state, isLoading) {
+            console.log({isLoading})
+            if (isLoading) {
+              state.refCount++;
+              state.isLoading = true;
+            } else if (state.refCount > 0) {
+              state.refCount--;
+              state.isLoading = (state.refCount > 0);
+            }
+          }
+        }
+      }
     }
   })
 }
