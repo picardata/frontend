8385c90689d95855c2217554e553e966e62f7a7c
Moch Deden <moch.deden@emewlabs.com>
2021-05-19 17:46:08 +0700

    check for empty in owner name & email value before display it

--- components/Application/Hubspot/HubspotDealListWidget.vue
+++ components/Application/Hubspot/HubspotDealListWidget.vue
@@ -53,7 +53,7 @@
           <template v-slot="{row}">
             <div class="media align-items-center">
               <div class="media-body">
-                <span class="name mb-0 text-sm">{{ row.ownerName }} ({{ row.ownerEmail }})</span>
+                <span class="name mb-0 text-sm">{{createOwnerName(row)}}</span>
               </div>
             </div>
           </template>
@@ -184,6 +184,17 @@ export default {
       })
   },
   methods: {
+    createOwnerName(owner) {
+      if(owner.ownerName && owner.ownerEmail) {
+        return `${owner.ownerName} ( ${owner.ownerEmail} )`;
+      } else if(owner.ownerName) {
+        return owner.ownerName;
+      } else if(owner.ownerEmail) {
+        return owner.ownerEmail;
+      } else {
+        return '';
+      }
+    },
     async saveGroup () {
       try {
         console.log('close date = ')
