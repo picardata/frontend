0bc39e03ddb692469c34f4dcecf3876569af6d65
Moch Deden <moch.deden@emewlabs.com>
2021-05-21 13:56:51 +0700

    Add stripe callback

--- pages/apps/callback/stripe/index.vue
+++ pages/apps/callback/stripe/index.vue
@@ -10,14 +10,13 @@ export default {
   layout: 'argon',
   components: { RedirectionContent },
   mounted () {
-    console.log('Masuk sini bro !')
-    // this.$axios.get('/api/integrations/slack/oauth2/callback?code=' + this.$route.query.code)
-    //   .then(() => {
-    //     this.$router.push('/apps/integrated-apps')
-    //   }).catch(
-    //   // eslint-disable-next-line no-console
-    //     (e) => { console.log(e) }
-    //   )
+    this.$axios.get('/api/integrations/stripe/oauth2/callback?code=' + this.$route.query.code)
+      .then(() => {
+        this.$router.push('/apps/integrated-apps')
+      }).catch(
+      // eslint-disable-next-line no-console
+        (e) => { console.log(e) }
+      )
   }
 }
 </script>
