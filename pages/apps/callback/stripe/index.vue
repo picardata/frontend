b39c6f255910fa71c037b3d033fde19663c53660
Moch Deden <moch.deden@emewlabs.com>
2021-05-21 11:54:54 +0700

    Init stripe callback

---
+++ pages/apps/callback/stripe/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RedirectionContent />
+  </div>
+</template>
+
+<script>
+import RedirectionContent from '@/components/Application/RedirectionContent'
+export default {
+  layout: 'argon',
+  components: { RedirectionContent },
+  mounted () {
+    console.log('Masuk sini bro !')
+    // this.$axios.get('/api/integrations/slack/oauth2/callback?code=' + this.$route.query.code)
+    //   .then(() => {
+    //     this.$router.push('/apps/integrated-apps')
+    //   }).catch(
+    //   // eslint-disable-next-line no-console
+    //     (e) => { console.log(e) }
+    //   )
+  }
+}
+</script>
