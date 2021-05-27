5d60ba719298c42a0444d74ac2e9184fac49d758
randy.faturrakhman@gmail.com <randy.faturrakhman@gmail.com>
2021-05-27 16:37:48 -0400

    PCDT-67: Survey form

--- pages/forms/survey/_id.vue
+++ pages/forms/survey/_id.vue
@@ -26,6 +26,7 @@
               <h3 v-if="field.required == 1" class="d-inline" style="color: red">
                 *
               </h3>
+              <label v-if="shortField[index].description !== ''" class="d-block text-muted" style="font-size: 12px;">{{ shortField[index].description }}</label>
               <div v-if="field.type === 0" class="form-group">
                 <input
                   v-model="answers[index].name"
@@ -85,11 +86,77 @@
               <div v-if="field.type === 4" class="form-group">
                 <div>
                   <br>
-                  <select v-model="answers[index].name" class="form-select" :name="formName(field.name)">
+                  <select v-model="answers[index].name" class="form-control form-select" :name="formName(field.name)">
                     <option v-for="choice in field.fieldChoices" :key="choice.id" :value="choice.name">
                       {{ choice.name }}
                     </option>
                   </select>
+                </div>
+              </div>
+              <div v-if="field.type === 5" class="form-group">
+                <div style="width:25em" class="file-upload">
+                  <br>
+                  <div class="custom-file">
+                    <input
+                        id="customFileLang"
+                        type="file"
+                        class="custom-file-input"
+                        lang="en"
+                        @change="setFiles(index)"
+                    >
+                    <label class="custom-file-label" for="customFileLang">
+                      {{ label }}
+                    </label>
+                  </div>
+                  <label class="mt-2 text-muted ">Allowing types :</label>
+                  <ul class="list-type text-muted ">
+                    <li v-for="(checkbox, check_key) in shortField[index].uploads.checkboxValue" :key="check_key" class="text-muted ">
+                      {{ types[checkbox-1].name }} <span v-if="check_key < (shortField[index].uploads.checkboxValue.length - 1)">,</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div v-if="field.type === 6" class="form-group">
+                <div style="width:32em;" class="linear-scale scroll-horizontal">
+                  <br>
+                  <div class="text-center">
+                    <div class="d-inline">
+                      {{ shortField[index].linear.label1 }}
+                    </div>
+                    <div v-for="(n,n_key) in shortField[index].linear.toValue" :key="n_key" class="d-inline">
+                      <base-radio
+                          v-model="answers[index].scale"
+                          :name="n"
+                          class="mb-3 d-inline"
+                      >
+                        {{ n }}
+                      </base-radio>
+                    </div>
+                    <div class="d-inline ml-4">
+                      {{ shortField[index].linear.label2 }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div v-if="field.type === 7" class="form-group">
+                <div style="width: 25em" class="field-dates">
+                  <br>
+                  <base-input
+                      id="example-date-input"
+                      v-model="answers[index].date"
+                      type="date"
+                      placeholder="Day, month, year"
+                  />
+                </div>
+              </div>
+              <div v-if="field.type === 8" class="form-group">
+                <div style="width: 25em" class="field-dates">
+                  <br>
+                  <base-input
+                      id="example-time-input"
+                      v-model="answers[index].time"
+                      type="time"
+                  />
                 </div>
               </div>
             </div>
@@ -132,31 +199,135 @@ export default {
         data.form.id = data.id
         data.form.email = data.email
         data.form.answers = []
+        data.form.shortField = []
         data.form.fields = data.form.fields.filter((field) => {
           field.errors = []
           field.fieldChoices = field.fieldChoices.filter((choice) => {
             return choice.status === 1
           })
+          field.fieldTexts = field.fieldTexts.filter((y) => {
+            return y.status === 1
+          })
+          field.fieldUploads = field.fieldUploads.filter((y) => {
+            y.checkboxValue = JSON.parse(y.checkboxValue)
+            return y.status === 1
+          })
+          field.fieldLinearScales = field.fieldLinearScales.filter((y) => {
+            return y.status === 1
+          })
+          field.fieldDates = field.fieldDates.filter((y) => {
+            return y.status === 1
+          })
           return field.status === 1
         })
 
         data.form.fields.map((field) => {
-          if (field.type === 3) {
+          if (field.type === 0 || field.type === 1) {
             data.form.answers.push({
               fieldId: field.id,
+              type: field.type,
+              name: '',
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              description: field.fieldTexts[0].description,
+              image: field.fieldTexts[0].image
+            })
+          } else if (field.type === 3) {
+            data.form.answers.push({
+              fieldId: field.id,
+              type: field.type,
               name: [],
-              other: ''
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              description: '',
+              image: ''
             })
           } else if (field.type === 4) {
             data.form.answers.push({
               fieldId: field.id,
-              name: field.fieldChoices[0].name
+              type: field.type,
+              name: field.fieldChoices[0].name,
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              description: '',
+              image: ''
+            })
+          } else if (field.type === 5) {
+            data.form.answers.push({
+              fieldId: field.id,
+              type: field.type,
+              name: '',
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              uploads: field.fieldUploads[0],
+              description: field.fieldUploads[0].description,
+              image: field.fieldUploads[0].image
+            })
+          } else if (field.type === 6) {
+            data.form.answers.push({
+              fieldId: field.id,
+              type: field.type,
+              name: '',
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              linear: field.fieldLinearScales[0],
+              description: field.fieldLinearScales[0].description,
+              image: field.fieldLinearScales[0].image
+            })
+          } else if (field.type === 7 || field.type === 8) {
+            data.form.answers.push({
+              fieldId: field.id,
+              type: field.type,
+              name: '',
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              date: field.fieldDates[0],
+              description: field.fieldDates[0].description,
+              image: field.fieldDates[0].image
             })
           } else {
             data.form.answers.push({
               fieldId: field.id,
+              type: field.type,
               name: '',
-              other: ''
+              other: '',
+              files: '',
+              scale: '',
+              date: '',
+              time: ''
+            })
+            data.form.shortField.push({
+              description: '',
+              image: ''
             })
           }
         })
@@ -164,6 +335,54 @@ export default {
         return data.form
       })
       .catch(e => console.log(e))
+  },
+  data() {
+    return {
+      types: [
+        {
+          name: 'Document',
+          id: 1
+        },
+        {
+          name: 'PDF',
+          id: 2
+        },
+        {
+          name: 'Drawing',
+          id: 3
+        },
+        {
+          name: 'Video',
+          id: 4
+        },
+        {
+          name: 'Spreadsheet',
+          id: 5
+        },
+        {
+          name: 'Presentation',
+          id: 6
+        },
+        {
+          name: 'Image',
+          id: 7
+        },
+        {
+          name: 'Audio',
+          id: 8
+        }
+      ],
+      files: []
+    }
+  },
+  computed: {
+    label () {
+      const fileNames = []
+      for (const file of this.files) {
+        fileNames.push(file.name)
+      }
+      return fileNames.length ? fileNames.join(', ') : 'Select file'
+    }
   },
   methods: {
     extractChoiceObject (choice) {
@@ -177,6 +396,10 @@ export default {
       if (errors === 0) {
         await this.answers.map((v, i) => {
           let name = v.name
+          let files = v.files
+          let scale = v.scale
+          let date = v.date
+          let time = v.time
           if (typeof name === 'object') {
             name = name.map((a) => {
               if (a === 'Other') {
@@ -188,36 +411,53 @@ export default {
           } else {
             name = name === 'Other' ? v.other : name
           }
-          name = [...new Set(name)]
-
+          name = typeof name === 'object' ? [...new Set(name)] : name
           this.$axios.$post('/api/field-answers/', {
             name: typeof name === 'object' ? name.join(',') : name,
+            fileName: typeof files === 'object' ? files[0].name : '',
+            scale: scale,
+            date: date,
+            time: time,
             formRespondent: this.$route.params.id,
             field: this.fields[i].id
           })
-            .then(() => {
-              this.$router.push('/forms/survey/thank-you?form=' + this.id)
-            })
-            .catch(res => console.log(res))
+              .then(() => {
+                this.$router.push('/forms/survey/thank-you?form=' + this.id)
+              })
+              .catch(res => console.log(res))
         })
       }
     },
     checkRequiredFields () {
       let warning = 0
       for (const i in this.fields) {
-        if (this.falsyValue(this.answers[i].name) && this.fields[i].required) {
+        if (this.whichField(this.fields[i].type, i) && this.fields[i].required) {
           this.fields[i].errors.push('This form is required')
           warning++
         }
       }
       return warning
     },
-    falsyValue (value) {
-      if (typeof value === 'object') {
+    whichField (type, i) {
+      if (type < 5) { return this.falsyValue(this.answers[i].name, type) }
+      if (type === 5) { return this.falsyValue(this.answers[i].files, type) }
+      if (type === 6) { return this.falsyValue(this.answers[i].scale, type) }
+      if (type === 7) { return this.falsyValue(this.answers[i].date, type) }
+      if (type === 8) { return this.falsyValue(this.answers[i].time, type) }
+      return false
+    },
+    falsyValue (value, type) {
+      if (typeof value === 'object' && type !== 5) {
         return !value.map(x => x).length
+      } else if (type === 5 && typeof value === 'object' && value[0].length > 0) {
+        return !value
       } else {
         return !value
       }
+    },
+    setFiles (index) {
+      this.files = event.target.files
+      this.answers[index].files = this.files
     }
   }
 }
@@ -251,5 +491,38 @@ input.form-control:focus {
   font-weight: normal;
   font-size: 16px;
   padding: 15px 30px;
+}
+.file-upload label.text-muted{
+  font-size: 12px;
+}
+.file-upload .custom-file-label{
+  overflow-x: hidden;
+  overflow-y: hidden;
+  white-space: nowrap;
+}
+.list-type {
+  display: inline;
+  padding-left:5px;
+}
+.list-type li{
+  display:inline;
+  list-style: none;
+  font-size: 12px;
+}
+.linear-scale{
+  height: 85px;
+}
+.scroll-horizontal{
+  white-space: nowrap;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+</style>
+
+<style>
+.linear-scale .custom-control-label::before,
+.linear-scale .custom-control-label::after{
+  left: -3px !important;
+  top: 25px !important;
 }
 </style>
