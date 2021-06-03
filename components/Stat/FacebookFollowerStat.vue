40bcaa8fa4f753e4bf572f50cbccff033dd0182d
Moch Deden <moch.deden@emewlabs.com>
2021-06-03 16:01:43 +0700

    Split facebook follower stat to its own component

---
+++ components/Stat/FacebookFollowerStat.vue
@@ -0,0 +1,14 @@
+<template>
+    <stats-card
+        title="Total Facebook Page Follower"
+        type="gradient-info"
+        sub-title="200"
+        icon="ni ni-chart-bar-32">
+        <template slot="footer">
+        <span class="text-nowrap">Since last month</span>
+        </template>
+    </stats-card>
+</template>
+
+<script>
+</script>
