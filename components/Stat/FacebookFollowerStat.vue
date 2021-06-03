aefed9ad3a53c0993040597b21e90738bc901386
Moch Deden <moch.deden@emewlabs.com>
2021-06-03 16:15:33 +0700

    Call facebook page follower

--- components/Stat/FacebookFollowerStat.vue
+++ components/Stat/FacebookFollowerStat.vue
@@ -2,7 +2,7 @@
     <stats-card
         title="Total Facebook Page Follower"
         type="gradient-info"
-        sub-title="200"
+        :sub-title="this.facebookFollowerCount"
         icon="ni ni-chart-bar-32">
         <template slot="footer">
         <span class="text-nowrap">Since last month</span>
@@ -11,4 +11,21 @@
 </template>
 
 <script>
+import StatsCard from '~/components/argon-core/Cards/StatsCard'
+
+export default {
+  components: {
+    StatsCard
+  },
+  async fetch () {
+    const pageFollowerResponse = await this.$axios.$get('/api/facebook/page-followers')
+
+    this.facebookFollowerCount = pageFollowerResponse.followers_count
+  },
+  data () {
+    return {
+      facebookFollowerCount: 0 
+    }
+  }
+}
 </script>
