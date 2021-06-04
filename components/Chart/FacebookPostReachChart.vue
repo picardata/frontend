a937d02fc2c1f13bc5bf7474450999fe951c821d
Moch Deden <moch.deden@emewlabs.com>
2021-06-04 15:38:33 +0700

    Split facebook post reach to its own component

---
+++ components/Chart/FacebookPostReachChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <line-chart
+        :height="350"
+        :chart-data="chartData"
+    />
+</template>
+
+<script>
+import LineChart from '~/components/argon-core/Charts/LineChart'
+import { Charts } from '~/components/argon-core/Charts/config'
+
+export default {
+  components: {
+    LineChart
+  },
+  async fetch () {
+    const totalIntegrationResult = await this.$axios.$get('/api/integrations/total-users/stats')
+  },
+  data () {
+    return {
+        chartData: {
+            labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+                label: 'Performance',
+                data: [100, 20, 10, 30, 15, 40, 20, 60, 60],
+                borderColor: Charts.colors.theme.primary
+            }]
+        }
+    }
+  }
+}
+</script>
