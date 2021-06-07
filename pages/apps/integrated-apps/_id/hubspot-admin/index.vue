b3cd71ae9ef34a84f9fab2d13762d8053248236c
aakhyar <akhyar.adhin@emewlabs.com>
2021-06-07 07:58:16 +0700

    PCDT-102: mixin for hubspot chart deals
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>

--- pages/apps/integrated-apps/_id/hubspot-admin/index.vue
+++ pages/apps/integrated-apps/_id/hubspot-admin/index.vue
@@ -59,6 +59,7 @@ import HubspotContactCount from '@/components/Application/Hubspot/HubspotContact
 import HubspotDealChart from '@/components/Application/Hubspot/HubspotDealChart'
 import ApplicationDetail from '~/components/Application/ApplicationDetail'
 import loaderMixin from '~/mixins/loader'
+import hubspotMixin from '~/mixins/hubspot'
 
 export default {
   layout: 'argon',
@@ -73,7 +74,8 @@ export default {
     HubspotDealChart
   },
   mixins: [
-    loaderMixin
+    loaderMixin,
+    hubspotMixin
   ],
   async asyncData (context) {
     return await context.app.$axios.get('/api/integrations/' + context.route.params.id)
@@ -98,46 +100,11 @@ export default {
           name: 'Hubspot',
           path: '/apps/integrated-apps'
         }
-      ],
-      dealsChart: {
-        dealStage: 'allstages',
-        loaded: false,
-        labels: [],
-        datasets: [{ data: [] }]
-      }
+      ]
     }
   },
   mounted () {
-    this.getDealsData()
-  },
-  methods: {
-    async getDealsData () {
-      const labels = []
-      const chartData = []
-      await this.$axios.get('/api/hubspot/deals/stats', {
-        params: {
-          month: this.$moment().format('M'),
-          year: this.$moment().format('Y')
-        }
-      })
-        .then((data) => {
-          data.data.map((deal) => {
-            const l = deal.dealStage
-            if (!labels.includes(l)) {
-              labels.push(l)
-              chartData.push(deal.total)
-            } else {
-              const i = labels.indexOf(l)
-              chartData[i] += deal.total
-            }
-          })
-
-          this.dealsChart.labels = labels
-          this.dealsChart.datasets[0].data = chartData
-
-          this.dealsChart.loaded = true
-        })
-    }
+    this.getHubspotChartDealsData()
   }
 }
 </script>
