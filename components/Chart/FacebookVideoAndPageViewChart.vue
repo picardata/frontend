f3f9db734d14f20e85375938ae098bcd1f006b22
Moch Deden <moch.deden@emewlabs.com>
2021-06-08 14:31:23 +0700

    User real data

--- components/Chart/FacebookVideoAndPageViewChart.vue
+++ components/Chart/FacebookVideoAndPageViewChart.vue
@@ -1,8 +1,8 @@
 <template>
   <bar-chart
     :height="350"
-    :chart-data="videoAndPageView.chartData"
-    :extra-options="videoAndPageView.extraOptions"
+    :chart-data="this.chartData"
+    :extra-options="this.extraOptions"
   />
 </template>
 
@@ -15,88 +15,63 @@ function randomScalingFactor () {
   return Math.round(Math.random() * 100)
 }
 
+function getResponseData(dataParam) {
+    if (dataParam.length > 0) {
+      const dataRaw = dataParam[0]
+      const values = dataRaw.values
+
+      const labels = values.map(value => moment(value.end_time.date).format('MMM DD'))
+      const data = values.map(value => value.value)
+
+      return {
+        labels,
+        data
+      }
+    }
+
+    return null;
+}
+
 export default {
   components: {
     BarChart
   },
   async fetch () {
-    // const postReachsResponse = await this.$axios.$get('/api/facebook/post-reach')
+    const pageViewResponse = await this.$axios.$get('/api/facebook/page-views')
+    const pageVideoViewResponse = await this.$axios.$get('/api/facebook/videos-views')
 
-    // if (postReachsResponse.length > 0) {
-    //   const postReachData = postReachsResponse[0]
-    //   const values = postReachData.values
+    const viewResponse = getResponseData(pageViewResponse)
+    const videoViewRespone = getResponseData(pageVideoViewResponse)
 
-    //   const labels = values.map(value => moment(value.end_time.date).format('MMM DD'))
-    //   const data = values.map(value => value.value)
-
-    //   this.chartData = {
-    //     labels,
-    //     datasets: [{
-    //       label: 'Post Reach',
-    //       data,
-    //       borderColor: Charts.colors.theme.primary
-    //     }]
-    //   }
-    // }
+    this.chartData = {
+      labels: viewResponse.labels,
+      datasets: [{
+        label: 'Page View',
+        backgroundColor: Charts.colors.theme.danger,
+        data: viewResponse.data
+      }, {
+        label: 'Page Video View',
+        backgroundColor: Charts.colors.theme.primary,
+        data: videoViewRespone.data
+      }]
+    }
   },
   data () {
     return {
-      videoAndPageView: {
-        chartData: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-            label: 'Dataset 1',
-            backgroundColor: Charts.colors.theme.danger,
-            data: [
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor()
-            ]
-          }, {
-            label: 'Dataset 2',
-            backgroundColor: Charts.colors.theme.primary,
-            data: [
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor()
-            ]
-          }, {
-            label: 'Dataset 3',
-            backgroundColor: Charts.colors.theme.success,
-            data: [
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor()
-            ]
-          }]
-
+      chartData: null,
+      extraOptions: {
+        tooltips: {
+          mode: 'index',
+          intersect: false
         },
-        extraOptions: {
-          tooltips: {
-            mode: 'index',
-            intersect: false
-          },
-          responsive: true,
-          scales: {
-            xAxes: [{
-              stacked: true
-            }],
-            yAxes: [{
-              stacked: true
-            }]
-          }
+        responsive: true,
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            stacked: true
+          }]
         }
       }
     }
