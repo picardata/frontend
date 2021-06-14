6f6c428fe4c0cd5e2c44872c079e4c18d6a4b41f
aakhyar <akhyar.adhin@emewlabs.com>
2021-06-14 16:08:42 +0700

    PCDT-102: mixin for hubspot chart deals
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>
    
    # Conflicts:
    #	pages/apps/integrated-apps/_id/hubspot-admin/index.vue

--- pages/apps/integrated-apps/_id/hubspot-admin/index.vue
+++ pages/apps/integrated-apps/_id/hubspot-admin/index.vue
@@ -100,45 +100,7 @@ export default {
           name: 'Hubspot',
           path: '/apps/integrated-apps'
         }
-      ],
-      dealsChart: {
-        dealStage: 'allstages',
-        loaded: false,
-        labels: [],
-        datasets: [{ data: [] }]
-      }
-    }
-  },
-  mounted () {
-    this.getDealsData()
-  },
-  methods: {
-    async getDealsData () {
-      const labels = []
-      const chartData = []
-      await this.$axios.get('/api/hubspot/deals/stats', {
-        params: {
-          month: this.$moment().format('M'),
-          year: this.$moment().format('Y')
-        }
-      })
-        .then((data) => {
-          data.data.map((deal) => {
-            const l = deal.dealStage
-            if (!labels.includes(l)) {
-              labels.push(l)
-              chartData.push(deal.total)
-            } else {
-              const i = labels.indexOf(l)
-              chartData[i] += deal.total
-            }
-          })
-
-          this.dealsChart.labels = labels
-          this.dealsChart.datasets[0].data = chartData
-
-          this.dealsChart.loaded = true
-        })
+      ]
     }
   },
   mounted () {
