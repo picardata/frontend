71823ae67bdee7d706679294a546407b0db1b1a1
Ilham <fajarmuhammadilham@gmail.com>
2021-06-21 08:41:52 +0700

    PCDTPRO-67: Fix component back button

--- components/PrevPage.vue
+++ components/PrevPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row prev">
     <div class="col-12">
-      <a class="btn p-0" href="#" @click.prevent="$router.go(-1)">
+      <a class="btn p-0" href="#" @click.prevent="onClick">
         <font-awesome-icon fixed-width size="2x" class="text-default p-0" :icon="['fas', 'arrow-left']" />
       </a>
     </div>
@@ -10,7 +10,17 @@
 
 <script>
 export default {
-  name: 'PrevPage'
+  name: 'PrevPage',
+  props: ['destination'],
+  methods: {
+    onClick () {
+      if (this.destination) {
+        window.location.href = this.destination
+      } else {
+        this.$router.go(-1)
+      }
+    }
+  }
 }
 </script>
 
