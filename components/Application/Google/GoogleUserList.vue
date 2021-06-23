03e3e03fc893c641dd381d0eb1f4f8b1ae05ebf5
aakhyar <79188053+aakhyar@users.noreply.github.com>
2021-06-23 17:17:28 +0700

    PCDTPRO-24: get data from db (#349)
    * PCDTPRO-24: get data from db
    
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>
    
    * PCDTPRO-24: store into variable
    
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>
    
    * PCDTPRO-24: lint
    
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>
    
    * PCDTPRO-24: add try catch
    
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>

--- components/Application/Google/GoogleUserList.vue
+++ components/Application/Google/GoogleUserList.vue
@@ -143,17 +143,30 @@
 export default {
   name: 'GoogleUserList',
   async fetch () {
-    await this.$axios.get('/api/google-directories/get-groups-and-members')
-      .then((data) => {
-        // eslint-disable-next-line no-console
-        console.log(data.data.groups)
-        this.groups = data.data.groups
-      }).catch(
-        (e) => {
-        // eslint-disable-next-line no-console
-          console.log(e)
-        }
-      )
+    try {
+      const data = await this.$axios.get('/api/integration-groups/')
+
+      this.groups = data.data.map((group) => {
+        group.id = group.groupId
+        delete group.groupId
+        group.members = group.integrationGroupMembers.map((member) => {
+          member.id = member.integrationUser.userId
+          member.email = member.integrationUser.email
+          member.name = {
+            familyName: member.integrationUser.familyName,
+            fullName: member.integrationUser.fullName,
+            givenName: member.integrationUser.givenName
+          }
+          member.role = member.role ? member.role : 'MEMBER'
+          member.isAMember = true
+          delete member.integrationUser
+          delete member.integrationGroup
+          return member
+        })
+        delete group.integrationGroupMembers
+        return group
+      })
+    } catch (e) {}
   },
   data () {
     return {
---
+++ components/Modal/authorize-integrate-app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <header class="modal-header">
+      <h5 id="exampleModalLabel" class="modal-title" />
+      <button type="button" data-dismiss="modal" aria-label="Close" class="close" @click="$emit('dismissModal')">
+        <span class="pd-icon pdicon-Cross" />
+      </button>
+    </header>
+    <div>
+      <img
+        slot="image"
+        class="card-img-top card-img-logo"
+        :src="app.logo"
+        alt="Card image cap"
+      >
+      <p class="mt-3 notify-text font-weight-bolder title-text">
+        Picardata wants to access your <span class="text-primary">{{ app.name }}</span> account
+      </p>
+      <p class="content-text">
+        This will allow Picardata to:
+      </p>
+      <ul class="content-text">
+        <li>View and access your data <i class="pd-icon pdicon-Alert-info" /></li>
+      </ul>
+    </div>
+    <footer slot="modal-footer">
+      <div class="w-100 mr-4 text-right">
+        <base-button tag="a" type="primary" target="_blank" :href="app.oauthUrl">
+          Integrate now
+        </base-button>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AuthorizeIntegrateModal',
+  props: {
+    app: Object,
+    dismissModal: Function
+  }
+}
+</script>
