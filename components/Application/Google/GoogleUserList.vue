9427acc1095f65a359396977742a35232e882fcb
aakhyar <akhyar.adhin@emewlabs.com>
2021-06-23 07:37:08 +0700

    PCDTPRO-24: get data from db
    Signed-off-by: aakhyar <akhyar.adhin@emewlabs.com>

--- components/Application/Google/GoogleUserList.vue
+++ components/Application/Google/GoogleUserList.vue
@@ -143,11 +143,29 @@
 export default {
   name: 'GoogleUserList',
   async fetch () {
-    await this.$axios.get('/api/google-directories/get-groups-and-members')
+    await this.$axios.get('/api/integration-groups/')
       .then((data) => {
-        // eslint-disable-next-line no-console
-        console.log(data.data.groups)
-        this.groups = data.data.groups
+        this.groups = data.data.map((group) => {
+          group.id = group.groupId
+          delete group.groupId
+          group.members = group.integrationGroupMembers.map((member) => {
+            member.id = member.integrationUser.userId
+            member.email = member.integrationUser.email
+            member.name = {
+              familyName: member.integrationUser.familyName,
+              fullName: member.integrationUser.fullName,
+              givenName: member.integrationUser.givenName
+            }
+            member.role = member.role ? member.role : 'MEMBER'
+            member.isAMember = true
+            delete member.integrationUser
+            delete member.integrationGroup
+            return member
+          })
+          delete group.integrationGroupMembers
+          return group
+        })
+        console.log(this.groups)
       }).catch(
         (e) => {
         // eslint-disable-next-line no-console
