f3fedc5c681f0c5afdb4c531e333553f912c90e8
mchew8888 <michael.chew2806@gmail.com>
2021-09-14 17:42:17 +0800

    Redirect to home page upon user login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -183,7 +183,7 @@ export default {
         if (!this.isLogin) {
           this.$router.push('/onboarding')
         } else {
-          this.$router.push('/apps/integrated-apps')
+          this.$router.push('/')
         }
       } catch (err) {
         this.errors.password = '<i class="fa fa-exclamation-circle"></i> Email or password  you entered is incorrect'
